5723a7ad778fb89c1a8a862e9cb21a9fc2f7aa90
qroux <quentin.roux@hotmail.fr>
2021-04-18 18:48:46 +0200

    SwaggerUi working with default config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { hostname } from 'os';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors({
     origin: 'http://localhost:4200',
     methods: 'GET,POST,PATCH,DELETE',
   });
+
   await app.listen(3000);
 }
 bootstrap();
